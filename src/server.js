e504598f3218cfe66bc8f37396a699b3daf5337f
Mary Reaky <maryreaky@gmail.com>
2025-11-15 07:43:49 -0800

    fix(server): correct console.log string interpolation; bind public server to process.env.PORT

--- src/server.js
+++ src/server.js
@@ -1,8 +1,8 @@
-const http = require('http');
-const app = require('./app');
+const http = require("http");
+const app = require("./app");
 const PORT = process.env.PORT || 3000;
 const server = http.createServer(app);
 server.listen(PORT, () => {
-  console.log(App listening on 0.0.0.0:);
+  console.log(`App listening on 0.0.0.0:${PORT}`);
 });
 module.exports = server;
