61a0cf8700b7df7078ec7df779a0ebe05ec81bbf
Mary Reaky <maryreaky@gmail.com>
2025-11-17 06:35:07 -0800

    fix(queue): force add queue.js and use ioredis directly

---
+++ src/server/queue.js
@@ -0,0 +1,11 @@
+const { Queue } = require('bullmq');
+const Redis = require('ioredis');
+
+const connection = new Redis(process.env.REDIS_URL);
+
+function createQueue(name='betrix-jobs') {
+  console.log('[queue] initializing BullMQ queue:', name);
+  return new Queue(name, { connection });
+}
+
+module.exports = { createQueue };
