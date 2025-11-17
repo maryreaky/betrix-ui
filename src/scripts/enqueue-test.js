828a4697c72e9819125c67c048336b1e09e59fe0
Mary Reaky <maryreaky@gmail.com>
2025-11-17 07:56:40 -0800

    fix(enqueue): use createQueue API; add retries & backoff

--- src/scripts/enqueue-test.js
+++ src/scripts/enqueue-test.js
@@ -1,7 +1,7 @@
-const { getQueue } = require('../server/queue');
+const { createQueue } = require('../server/queue');
 
 (async () => {
-  const queue = getQueue('betrix-jobs');
+  const queue = createQueue('betrix-jobs');
   const job = await queue.add('hello-world', { ts: Date.now(), from: 'enqueue-test' }, {
     attempts: 5,
     backoff: { type: 'exponential', delay: 1000 },
